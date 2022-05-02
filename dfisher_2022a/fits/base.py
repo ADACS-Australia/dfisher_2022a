b11b7351ad48e381b3e33d82ea9762254e65fdb0
JHu <jitinghu@swin.edu.au>
2022-05-02 14:51:10 +1000

    Implement parallel model fitting on spaxels

--- dfisher_2022a/fits/base.py
+++ dfisher_2022a/fits/base.py
@@ -1,7 +1,9 @@
 # interface between cube and model
 import numpy as np
 import pandas as pd
+import os
 from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from ..io.cube import FitReadyCube
 from ..models import Lm_Const_1GaussModel
@@ -13,6 +15,7 @@ class FitCube():
         self.var = cube.var
         self._get_weight()
         self.model = model
+        self._create_results_placeholder()
 
     def _get_weight(self):
         if self.var is not None:
@@ -31,33 +34,54 @@ class FitCube():
         params = m.guess(spaxel, self.x)
 
         result = m.fit(spaxel, params, x=self.x, weights=sp_weight)
+        name = os.getpid()
+        print("subprocess: ", name, " pixel: ", i, j)
 
         # type: lmfit ModelResult 
         return result
 
-    def _fit_single_index(self,i):
-        axis_0 = self.data.shape[0]
+    def _create_results_placeholder(self):
         axis_x = self.data.shape[1]
         axis_y = self.data.shape[2]
-        flat_data = self.data.reshape(axis_0, axis_x*axis_y)
-        if self.weights is not None:
-            flat_weights = self.weights.reshape(axis_0, axis_x*axis_y)
-            single_weight = flat_weights[:,i]
-        else:
-            single_weight = None
-        single = flat_data[:,i]
+        res = np.zeros((axis_x, axis_y))
+        df = pd.DataFrame(res)
+        self.res = df
 
-        m = self.model()
-        params = m.guess(single, self.x)
+    # def _fit_single_index(self,i):
+    #     axis_0 = self.data.shape[0]
+    #     axis_x = self.data.shape[1]
+    #     axis_y = self.data.shape[2]
+    #     flat_data = self.data.reshape(axis_0, axis_x*axis_y)
+    #     if self.weights is not None:
+    #         flat_weights = self.weights.reshape(axis_0, axis_x*axis_y)
+    #         single_weight = flat_weights[:,i]
+    #     else:
+    #         single_weight = None
+    #     single = flat_data[:,i]
 
-        result = m.fit(single, params, x=self.x, weights=single_weight)
+    #     m = self.model()
+    #     params = m.guess(single, self.x)
+
+    #     result = m.fit(single, params, x=self.x, weights=single_weight)
 
 
     def fit_all(self, nprocess):
-        p = Pool(processes=nprocess)
         axis_x = self.data.shape[1]
         axis_y = self.data.shape[2]
-        p.map(self._fit_single_index, list(range(axis_x*axis_y)))
+        with ProcessPoolExecutor(max_workers=nprocess) as pool:
+            for i in range(axis_x):
+                for j in range(axis_y):
+                    name = os.getpid()
+                    future = pool.submit(self.fit_single_spaxel, i, j)
+                    self.res[i][j] = future.result()
+                   
+
+            
+
+        # p = Pool(processes=nprocess)
+        # axis_x = self.data.shape[1]
+        # axis_y = self.data.shape[2]
+        # p.map(self._fit_single_index, list(range(axis_x*axis_y)))
 
 
 
