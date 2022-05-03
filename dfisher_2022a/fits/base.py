5767e61a19d5e14e39ea347982a92c3a81cd5f09
JHu <jitinghu@swin.edu.au>
2022-05-04 06:56:08 +1000

    Separate mapping results to dataframe from parallel model fitting

--- dfisher_2022a/fits/base.py
+++ dfisher_2022a/fits/base.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 import os
 from multiprocessing import Pool
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+import threading
 from functools import partial
 from ..io.cube import FitReadyCube
 from ..models import Lm_Const_1GaussModel
@@ -24,28 +25,48 @@ class FitCube():
             weights = None
         self.weights = weights
 
-    def fit_single_spaxel(self, i, j):
+    def fit_single_spaxel(self, i, j, enable_res=False):
         spaxel = self.data[:, i, j]
+
         if self.weights is not None:
             sp_weight = self.weights[:,i,j]
         else:
             sp_weight = None
 
-        if spaxel.mask.all():
-            print("masked all data: ", i, j)
-            result = None
+        if enable_res == True:
+            axis_y = self.data.shape[2]
+            idx = i*axis_y + j
+            if spaxel.mask.all():
+                print("masked all data: ", i, j)
+                result = (idx, None)
 
+            else:                
+                m = self.model()
+                params = m.guess(spaxel, self.x)
+
+                result = (idx, m.fit(spaxel, params, x=self.x, weights=sp_weight))
+                name = os.getpid()
+                print("subprocess: ", name, " pixel: ", i, j)
+
+                # type: lmfit ModelResult 
+                
+            return result
         else:
-            m = self.model()
-            params = m.guess(spaxel, self.x)
+            if spaxel.mask.all():
+                print("masked all data: ", i, j)
+                result = None
 
-            result = m.fit(spaxel, params, x=self.x, weights=sp_weight)
-            name = os.getpid()
-            print("subprocess: ", name, " pixel: ", i, j)
+            else:
+                m = self.model()
+                params = m.guess(spaxel, self.x)
 
-            # type: lmfit ModelResult 
-            
-        return result
+                result = m.fit(spaxel, params, x=self.x, weights=sp_weight)
+                name = os.getpid()
+                print("subprocess: ", name, " pixel: ", i, j)
+
+                # type: lmfit ModelResult 
+                
+            return result
 
     def _create_results_placeholder(self):
         axis_x = self.data.shape[1]
@@ -57,29 +78,51 @@ class FitCube():
             df["row"][i*axis_y:(i+1)*axis_y] = i
             df["col"][i*axis_y:(i+1)*axis_y] = list(range(axis_y))
         
-        # df = df.astype({"row":np.int32, "col":np.int32})
         self.res = df
 
-    def fit_all(self, nprocess):
+    def _fit_all(self, nprocess):
         axis_x = self.data.shape[1]
         axis_y = self.data.shape[2]
         res_collect = []
         with ProcessPoolExecutor(max_workers=nprocess) as pool:
             for i in range(axis_x):
                 for j in range(axis_y):
-                    idx = i*axis_y + j
-                    future = pool.submit(self.fit_single_spaxel, i, j)
+                    future = pool.submit(self.fit_single_spaxel, i, j, enable_res=True)
                     # self.res["Result"][idx] = future.result()
                     res_collect.append(future)
-        return res_collect
-                   
+        
         #TODO: fit selected region
-            
 
         # p = Pool(processes=nprocess)
         # axis_x = self.data.shape[1]
         # axis_y = self.data.shape[2]
         # p.map(self._fit_single_index, list(range(axis_x*axis_y)))
+        return res_collect
+                   
+    def _map_result(self, res_ele):
+        name = threading.current_thread().getName()
+        print("threading: ", name)
+        future_result = res_ele.result()
+        idx = future_result[0]
+        self.res["Result"][idx] = future_result[1]
+
+
+    def _map_all(self, res_collect, nthread):
+        with ThreadPoolExecutor(max_workers=nthread) as pool:
+            for ele in res_collect:
+                pool.submit(self._map_result, ele)
+                
+
+     
+    def fit_all(self, nprocess, nthread):
+        res_collect = self._fit_all(nprocess = nprocess)
+        self._map_all(res_collect, nthread=nthread)
+
+
+
+
+
+        
 
 
 
