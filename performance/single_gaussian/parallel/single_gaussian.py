d2f46b6840aa5445589000097aecd97965e91c1b
JHu <jitinghu@swin.edu.au>
2022-07-25 22:12:01 +1000

    update performance test scripts to include all fitting procedures

--- performance/single_gaussian/parallel/single_gaussian.py
+++ performance/single_gaussian/parallel/single_gaussian.py
@@ -4,26 +4,43 @@
 from mpdaf.obj import Cube
 import time
 import dfisher_2022a as dfi
+from dfisher_2022a.emission_lines import Halpha
+from dfisher_2022a.io.cube import FitReadyCube, RestCube, SNRMap
+from dfisher_2022a.io.line import Line
 
 time_start = time.time()
+
+# 1. read in test data cube
 path = "./../../../dfisher_2022a/tests/data"
 file = path + "/" + "single_gaussian_muse_size.fits"
-
 cube = Cube(file)
 
+# 2. settings
+Z = 0.009482649107040553
+SNR_THRESHOLD = 5
 
+# 3. prepare data for fitting
+# 3.1 de-redshift
+rcube = RestCube(cube, Z)
+
+# 3.2 select fitting region
+fline = Line(Halpha)
+frcube = FitReadyCube(rcube, fline)
+
+# 3.3 get snr map
+snrmap = SNRMap(rcube.restcube, SNR_THRESHOLD)
+snr = snrmap.snr
+smap = snrmap.map
+
+# 3.4 get fit ready cube
+ffrcube = FitReadyCube(rcube, fline, SNR_THRESHOLD)
+
+# 4. fit the data
 model = dfi.models.Lm_Const_1GaussModel
 small = cube[:,100:120,100:120]
 # small = cube
-axis_x = small.data.shape[0]
-axis_y = small.data.shape[1]
-
-da = small.data
-print(type(cube.data))
-xwav = small.wave.coord()
 fc = dfi.fits.base.FitCube(small, model)
 time_m = time.time()
-# if __name__ == "__main__":
 
 # fit spaxels parallelly using multiprocessing
 fc.fit_all(4, 100)
--- performance/single_gaussian/serial/single_gaussian_serial.py
+++ performance/single_gaussian/serial/single_gaussian_serial.py
@@ -5,42 +5,51 @@ from mpdaf.obj import Cube
 import numpy as np
 import os
 import time
-from viztracer import VizTracer
-
 
 import dfisher_2022a as dfi
-   
-# tracer = VizTracer()
-# tracer.start()
+from dfisher_2022a.emission_lines import Halpha
+from dfisher_2022a.io.cube import FitReadyCube, RestCube, SNRMap
+from dfisher_2022a.io.line import Line
+
 time_start = time.time()
+
+# 1. read in test data cube
 path = "./../../../dfisher_2022a/tests/data"
 file = path + "/" + "single_gaussian_muse_size.fits"
-
 cube = Cube(file)
 
+# 2. settings
+Z = 0.009482649107040553
+SNR_THRESHOLD = 5
 
+# 3. prepare data for fitting
+# 3.1 de-redshift
+rcube = RestCube(cube, Z)
+
+# 3.2 select fitting region
+fline = Line(Halpha)
+frcube = FitReadyCube(rcube, fline)
+
+# 3.3 get snr map
+snrmap = SNRMap(rcube.restcube, SNR_THRESHOLD)
+snr = snrmap.snr
+smap = snrmap.map
+
+# 3.4 get fit ready cube
+ffrcube = FitReadyCube(rcube, fline, SNR_THRESHOLD)
+
+# 4. fit the data
 model = dfi.models.Lm_Const_1GaussModel
 small = cube[:, 100:120,100:120]
 # small = cube[:, 100:101,100]
 # small = cube
-axis_x = small.data.shape[0]
-axis_y = small.data.shape[1]
-
-da = small.data
-xwav = small.wave.coord()
 fc = dfi.fits.base.FitCube(small, model)
-
-
-# if __name__ == "__main__":
-# tracer = VizTracer(max_stack_depth=5)
-# tracer.start()
 
 # fit spaxel serially 
 fc.fit_all_serial()
 
-# tracer.stop()
-# tracer.save()
 time_m = time.time()
+
 print("--- %s seconds ---" % (time_m - time_start))
 
 
