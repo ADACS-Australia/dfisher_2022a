ef8c1b44961488b2fc296e486beecb166599c840
JHu <jitinghu@swin.edu.au>
2022-07-28 23:47:35 +1000

    refactor line class; add a runtime generated line dict

--- dfisher_2022a/__init__.py
+++ dfisher_2022a/__init__.py
@@ -1,8 +1,17 @@
 # __version__ = '0.1.0'
 
-from .io import cube, line
-from . import fits
-from . import models
-from . import emission_lines 
-
 __all__ = ["fits", "models", "emission_lines", "cube", "line"]
+
+# generate line dictionary at runtime
+def _generate_line_dict(emission_lines):
+    line_dict = {}
+    for attr in dir(emission_lines):
+        if not attr.startswith("_"):
+            line_dict[attr] = getattr(emission_lines,attr)
+    return line_dict
+    
+from . import emission_lines
+
+EmissionLines = _generate_line_dict(emission_lines)
+from . import fits, models
+from .io import cube, line
--- dfisher_2022a/io/line.py
+++ dfisher_2022a/io/line.py
@@ -1,19 +1,41 @@
-# emission line related
+from dataclasses import dataclass
 
+from dfisher_2022a import EmissionLines
+
+
+@dataclass(frozen=True)
 class Line():
     '''
-    determine the fitting region of a given line
+    object to store line information
     '''
-    def __init__(self, line, right=15, left=15) -> None:
+    name: str
+    @property
+    def wavelength(self):
+        return EmissionLines[self.name]
+ 
+class LineRegion():
+    def __init__(self, line:Line, right=15, left=15):
         self.line = line
         self.right = right
         self.left = left
+
+    @property
+    def low(self):
+        return self.line.wavelength - self.left
+
+    @property
+    def high(self):
+        return self.line.wavelength + self.right
+
+
+   # def __init__(self, name):
+    #     self.name = name
+    #     self.wavelength = EmissionLines[name]
+    # def __init__(self, line, right=15, left=15) -> None:
+    #     self.line = line
+    #     self.right = right
+    #     self.left = left
        
-        # get the fitting range in wavelength
-        self.low = line - left
-        self.high = line + right
-
-       
-    
-
-
+    #     # get the fitting range in wavelength
+    #     self.low = line - left
+    #     self.high = line + right
